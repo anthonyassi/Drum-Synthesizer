f6188897ba0559f8386ef2bbf29593ce7fd19a02
anthonyassi <aassi@ucsc.edu>
2014-11-09 17:25:06 -0800

    version 11 - sine noise osc

---
+++ version 11/Actions.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.Scanner;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+import static java.awt.BorderLayout.*;
+import javax.swing.JLabel;
+
+
+//ACTION LISTENER FOR SAMPLER BUTTONS
+class Actions implements ActionListener{
+ Sampler[] samplers;
+ Container pane;
+ int numSampler;
+ Actions(Sampler[] samplers,Container pane,int numSampler){
+  this.samplers = samplers;
+  this.pane = pane;
+  this.numSampler = numSampler;
+ }
+ //if an action event occurs some of this code will execute depending on the action event
+ public void actionPerformed(ActionEvent e){
+ // System.out.println(e.getActionCommand()); 
+  
+  String cmd = e.getActionCommand();
+  
+  //if user hits add button, then increment numSamplers and update the interface 
+    if(cmd.contains("ADD")){
+        Interface.numPanels++;
+        Interface.buildSynthesizer(pane);
+        }
+   //if user hits play button
+    if(cmd.contains("PLAY")){
+      System.out.println("Panel:"+numSampler+"PLAY");
+      //play sample
+      PlaySample p = new PlaySample();
+      p.play(samplers[numSampler].sample, samplers[numSampler].volumeSlider.getValue(), samplers[numSampler].cutoffSlider.getValue() );
+    }
+    if(cmd.contains("Open a File")){
+      System.out.println("OpenEnded");
+      int returnVal = samplers[numSampler].fc.showOpenDialog(null);
+      if(returnVal == JFileChooser.APPROVE_OPTION) {
+       System.out.println("You chose to open this file: " );
+           System.out.println( samplers[numSampler].fc.getSelectedFile().getName());
+           samplers[numSampler].sample = samplers[numSampler].fc.getSelectedFile();
+    }
+    }
+    
+}
+ 
+ 
+
+
+}
---
+++ version 11/Interface.java
@@ -0,0 +1,92 @@
+//VERSION 3
+//THIS VERSION STARTS WITH ONE PANEL WITH PLAY BUTTON 
+// AND VOLUME SLIDER THAT DONT DO ANYTHING. IMPLEMENTED 
+//"ADD" BUTTON THAT ADDS PANELS TO THE WINDOW.
+//FEEL FREE TO ADD, REPLACE, FIX CODE, JUST SAVE IT 
+//AS A NEW VERSION.
+
+
+import java.util.Scanner;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+import static java.awt.BorderLayout.*;
+import javax.swing.JLabel;
+
+class Interface{
+  static int numPanels;
+  static Sampler[] samplers = new Sampler[8];
+  static Synthesizer[] synthesizers = new Synthesizer[8];
+  public static void main(String[] args){
+  JFrame frame = new JFrame("Drum Synthesizer");
+  Container pane = frame.getContentPane();
+  
+  buildGUI(pane);
+  
+  frame.addWindowListener(new Exiter());
+  
+  frame.setSize(800,500);
+  frame.setVisible(true);
+  }
+  
+  
+  static void buildGUI(Container pane){
+    numPanels++;
+    //Create Initial sampler
+    samplers[0] = new Sampler();
+    //Create action listen and give it to addChannel button
+    Actions doAction = new Actions(samplers,pane,0);
+    Sampler.addChannel.addActionListener(doAction);
+    samplers[0].openButton.addActionListener(doAction);
+    samplers[0].play.addActionListener(doAction);
+    samplers[0].volumeSlider.addChangeListener(new SliderListener(samplers, pane, 0, 'v'));
+    samplers[0].pitchSlider.addChangeListener(new SliderListener(samplers, pane, 0, 'p'));
+    samplers[0].cutoffSlider.addChangeListener(new SliderListener(samplers, pane, 0, 'f'));
+    buildSampler(pane);
+    
+  }
+  
+  static void buildSampler(Container pane){
+   //if buildSampler is called from Actions class then creat new channel
+    if(numPanels>1){
+      int i = numPanels-1;
+      samplers[i]  = new Sampler();
+      samplers[i].openButton.addActionListener(new Actions(samplers,pane,i));
+      samplers[i].play.addActionListener(new Actions(samplers,pane,i));
+      samplers[i].volumeSlider.addChangeListener(new SliderListener(samplers, pane, i,'v'));
+      samplers[i].pitchSlider.addChangeListener(new SliderListener(samplers, pane, i,'p'));
+      samplers[i].cutoffSlider.addChangeListener(new SliderListener(samplers, pane, i,'f'));
+      samplers[0].add(samplers[i]);
+    }
+  
+    //Put addchannel button button under last channel
+    samplers[(numPanels-1)].add(Sampler.addChannel);
+    samplers[0].revalidate();
+    //add samplers to pane(window)
+    pane.add(samplers[0]);
+  }
+  
+    static void buildSynthesizer(Container pane){
+   //if buildSYNTH is called from Actions class then creat new channel
+    if(numPanels>1){
+      int i = numPanels-1;
+      synthesizers[i]  = new Synthesizer();
+      synthesizers[i].play.addActionListener(new SynthActions(synthesizers,pane,i));
+      synthesizers[i].sine.addActionListener(new SynthActions(synthesizers,pane,i));
+      synthesizers[i].noise.addActionListener(new SynthActions(synthesizers,pane,i));
+      synthesizers[i].volumeSlider.addChangeListener(new SynthListener(synthesizers, pane, i,'v'));
+      synthesizers[i].pitchSlider.addChangeListener(new SynthListener(synthesizers, pane, i,'p'));
+      synthesizers[i].cutoffSlider.addChangeListener(new SynthListener(synthesizers, pane, i,'f'));
+      samplers[0].add(synthesizers[i]);
+    }
+  
+    
+    //Put addchannel button button under last channel
+    synthesizers[(numPanels-1)].add(Sampler.addChannel);
+    samplers[0].revalidate();
+    //add samplers to pane(window)
+    pane.add(samplers[0]);
+  }
+
+
+}
